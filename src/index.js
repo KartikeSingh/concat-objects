cb99a3c270c1c83b5818c2e5cc875377cc5d018c
null <60427892+vierofernando@users.noreply.github.com>
2021-10-17 07:26:48 +0700

    fix(index): it's actually `every`, not `any`
    kill me

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
  * @exports Object the merged object
  */
 module.exports = function concatObjects(...objects) {
-    if (!objects.any(x => typeof x === "object"))
+    if (!objects.every(x => typeof x === "object"))
         throw new TypeError("Parameter must be an array of objects");
     
     return Object.fromEntries(objects.map(x => Object.entries(x)).reduce((a, b) => [...a, ...b]));
